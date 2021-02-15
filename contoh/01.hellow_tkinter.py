b252e2d106011791350694b24e3c45676913ab2c
Indar Sugiarto <indar.sugiarto@gmail.com>
2021-02-15 09:34:52 +0700

    Create 01.hellow_tkinter.py

---
+++ contoh/01.hellow_tkinter.py
@@ -0,0 +1,31 @@
+import tkinter as tk
+from tkinter import messagebox
+
+"""
+Alternatif GUI programming selain PyQt/PySide adalah tkInter.
+"""
+
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.hi_there = tk.Button(self)
+        self.hi_there["text"] = "Hello World\n(click me)"
+        self.hi_there["command"] = self.say_hi
+        self.hi_there.pack(side="top")
+
+        self.quit = tk.Button(self, text="QUIT", fg="red",
+                              command=self.master.destroy)
+        self.quit.pack(side="bottom")
+
+    def say_hi(self):
+        print("hi there, everyone!")
+        messagebox.showinfo("Say Hello", "Hello World")
+
+root = tk.Tk()
+app = Application(master=root)
+app.mainloop()
